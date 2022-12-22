3d68aef18de64ac7d969ea41e9564c4ba144df07
CorreaJo <j.victor.augusto3@gmail.com>
2022-12-22 15:46:45 -0300

    Arquivos Backend

---
+++ database/migrations/2022_12_22_173414_create_table_usuario.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableUsuario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email');
+            $table->string('senha');
+            $table->string('perfil');
+            $table->foreignId('idUnidade')->references('id')->on('unidade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_table__usuario');
+    }
+}
--- database/migrations/2022_12_22_174113_create_table_cupom.php
+++ database/migrations/2022_12_22_174113_create_table_cupom.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateTableCupom extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('cupom', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->date('dataRetirada');
+            $table->date('dataDisp');
+            $table->date('dataLimite');            
+            $table->string('status');
+            $table->foreignId('idBeneficiado')->references('id')->on('beneficiado')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('_table__cupom');
     }
 }
